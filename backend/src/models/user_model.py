7fa477ea691a1b388074ba8ffe8b477e4da1cc17
guilhermehbs <guilhermehbs100@gmail.com>
2025-06-16 19:03:32 -0300

    subindo

--- backend/src/models/user_model.py
+++ backend/src/models/user_model.py
@@ -1,8 +1,23 @@
 from .. import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from .receita_model import receitas_salvas
 
 class User(db.Model):
     __tablename__ = "usuarios" 
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column("nome_completo", db.String(80), nullable=False) 
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
 
+    receitas_salvas = db.relationship(
+        "Receita",
+        secondary=receitas_salvas,
+        backref="usuarios_que_salvaram"
+    )
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
