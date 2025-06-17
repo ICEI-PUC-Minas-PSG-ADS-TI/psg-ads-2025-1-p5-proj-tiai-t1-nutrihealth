6859216637317ca16f1f73069e4a6a0f80a6897a
guilhermehbs <guilhermehbs100@gmail.com>
2025-06-17 19:36:32 -0300

    atualizando

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,26 +1,34 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager 
+from flask_jwt_extended import JWTManager
 from .config import Config
 
 db = SQLAlchemy()
-jwt = JWTManager() 
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
-    jwt.init_app(app)  
+    jwt.init_app(app)
 
     with app.app_context():
         from .routes.user_route import user_bp
         from .routes.receita_route import receita_bp
+<<<<<<< Updated upstream
         from .routes.dashboard_route import relatorio_bp
 
         app.register_blueprint(user_bp)
         app.register_blueprint(receita_bp)
         app.register_blueprint(relatorio_bp)
+=======
+        from .routes.ingrediente_route import ingrediente_bp 
+
+        app.register_blueprint(user_bp)
+        app.register_blueprint(receita_bp)
+        app.register_blueprint(ingrediente_bp)
+>>>>>>> Stashed changes
 
         db.create_all()
 
